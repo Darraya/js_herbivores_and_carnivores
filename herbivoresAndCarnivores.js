4b9ff6d17a1a42539bee306ec1fb6f9aa3100984
Darraya <lurilun@meta.ua>
2025-06-17 13:20:05 +0300

    Updates

--- herbivoresAndCarnivores.js
+++ herbivoresAndCarnivores.js
@@ -2,21 +2,15 @@
 
 class Animal {
   static alive = [];
+
   constructor(name, health) {
-    this.health = health || 100;
+    this.health = health ?? 100;
     this.name = name;
+    Animal.alive.push(this);
   }
 
-  add(animal) {
-    Animal.alive.push(animal);
-  }
-
-  checkAlive(animal) {
-    if (animal.health <= 0) {
-      const index = Animal.alive.indexOf(animal);
-
-      Animal.alive.splice(index, 1);
-    }
+  static checkAlive() {
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
   }
 }
 
@@ -24,24 +18,18 @@ class Herbivore extends Animal {
   constructor(name, health) {
     super(name, health);
     this.hidden = false;
-    super.add(this);
   }
 
   hide() {
-    this.hidden = !this.hidden;
+    this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
-  constructor(name, health) {
-    super(name, health);
-    super.add(this);
-  }
-
   bite(animal) {
     if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
-      super.checkAlive(animal);
+      Animal.checkAlive();
     }
   }
 }
